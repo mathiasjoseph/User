71b44c749f9e83a6aca5fa5dbac7e730cf6e23a0
miky <mathias89100@gmail.com>
2017-05-30 21:46:13 +0200

    All

--- Model/BaseUser.php
+++ Model/BaseUser.php
@@ -15,4 +15,46 @@ use Miky\Component\Core\Model\CommonModelTrait;
 class BaseUser extends \FOS\UserBundle\Model\User implements CommonModelInterface
 {
     Use CommonModelTrait;
+
+    /**
+     * @var string
+     */
+    protected $lastname;
+
+    /**
+     * @var string
+     */
+    protected $firstname;
+
+    /**
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
 }
